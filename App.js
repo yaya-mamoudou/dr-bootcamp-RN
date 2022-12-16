b5c3d5487b0481c62e808053afaac13de1dbe37a
Yaya Mamoudou <57509871+yaya-mamoudou@users.noreply.github.com>
2022-12-16 14:49:00 +0100

    React native test

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, FlatList, Button, SafeAreaView, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [usernames, setUsernames] = useState(['Yaya', 'Mamoudou']);
+	const [user, setUser] = useState('');
+
+	const handleInput = (value) => {
+		setUser(value);
+	};
+
+	const addUser = () => {
+		setUsernames([user, ...usernames]);
+	};
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<TextInput
+				onChangeText={handleInput}
+				style={styles.input}
+				placeholder='Enter username'
+			/>
+			<Button onPress={addUser} title='Press me' />
+
+			<FlatList
+				data={usernames}
+				renderItem={({ item }) => {
+					return <Text style={styles.user}>{item}</Text>;
+				}}
+			/>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+	},
+	input: {
+		borderWidth: 4,
+		borderColor: '#ccc',
+		padding: 10,
+		marginHorizontal: 16,
+		borderRadius: 10,
+		fontSize: 18,
+	},
+	user: {
+		fontSize: 20,
+		textAlign: 'center',
+		marginTop: 20,
+	},
 });
